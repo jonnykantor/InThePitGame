ea05c975217046971f0db2748da0cbade3884747
jonnykantor <jonnykantor@gmail.com>
2016-06-17 13:49:43 -0700

    re-implemented foreground surfaces as sprites in a spritegroup

--- background_test.py
+++ background_test.py
@@ -4,68 +4,6 @@ from pygame.locals import *
 from random import randint
 from sys import argv
 
-if __name__ == "__main__": #GLOBALS HERE
-	##directions for movement
-	LEFT = 0
-	RIGHT = 1
-	DOWN = 2
-	UP = 3
-	#next two are to avoid animation of non-player objects 
-	#when they are only moving due to player movement
-	PLAYER_LEFT_ONLY = 4 
-	PLAYER_RIGHT_ONLY = 5
-	##/directions for movement
-
-	##colors
-	GREY = (128, 128, 128)
-	RED = (255, 0, 0)
-	GREEN = (0, 255, 0)
-	BLUE = (0, 0, 255)
-	LIGHT_BLUE = (125, 125, 255)
-	##/colors
-
-	##speeds
-	BACKGROUND_SPEED = 10
-	FOREGROUND_SPEED = 8
-	##/speeds
-
-	##collision rect widths
-	LEFT_RECT_WIDTH = 5
-	RIGHT_RECT_WIDTH = 5
-	##
-
-	##universal scenery heights
-	#remember that (0, 0) in pygame is the top-left corner; y > 0 is below this point
-	FLOOR_HEIGHT = 400
-	##
-
-	##image layout
-	#IDLE
-	IDLE_0 	= 0
-	IDLE_1 	= 1
-	IDLE_2 	= 2
-	IDLE_3 	= 3
-	#LEFT
-	LEFT_0 	= 4
-	LEFT_1 	= 5
-	LEFT_2 	= 6
-	LEFT_3 	= 7
-	#RIGHT
-	RIGHT_0 = 8
-	RIGHT_1 = 9
-	RIGHT_2 = 10
-	RIGHT_3 = 11
-	#UP
-	UP_0 	= 12
-	UP_1 	= 13
-	UP_2 	= 14
-	UP_3 	= 15
-	#DOWN
-	DOWN_0 	= 16
-	DOWN_1 	= 17
-	DOWN_2 	= 18
-	DOWN_3 	= 19	
-
 class CharacterObject(pygame.sprite.Sprite):
 	def __init__(self, screen, images, starting_position):
 		pygame.sprite.Sprite.__init__(self)
@@ -115,16 +53,14 @@ class CharacterObject(pygame.sprite.Sprite):
 		
 		collide_left = self.left_side_collision_rect.collidelist([x.rect for x in EVERY_CHARACTER_SPRITE_GROUP])
 		collide_right = self.right_side_collision_rect.collidelist([x.rect for x in EVERY_CHARACTER_SPRITE_GROUP])
-		collide_bottom = self.bottom_collision_rect.collidelist(FOREGROUND.scenery_rects)
+		collide_bottom = self.bottom_collision_rect.collidelist([x.rect for x in FOREGROUND_SPRITE_GROUP])
 		
 		if collide_left == -1: self.isCollidingLeft = False
 		else: self.isCollidingLeft = True
 		if collide_right == -1: self.isCollidingRight = False
 		else: self.isCollidingRight = True
 		if collide_bottom == -1: self.isCollidingBottom = False
-		else: 
-			self.isCollidingBottom = True
-			print "YES"
+		else: self.isCollidingBottom = True			
 		
 		if direction == UP and not self.isAirborne:	
 			self.jumpCounter = 5			
@@ -162,11 +98,11 @@ class CharacterObject(pygame.sprite.Sprite):
 							
 			#extending collision rectangle for object from self.rect.bottom to y = 400			
 			if collide_bottom != -1:				
-				if ( abs(self.rect.bottom - FOREGROUND.scenery_rects[collide_bottom].top) ) <= ( self.fall_speed + 2 ):
+				if ( abs(self.rect.bottom - [x.rect for x in FOREGROUND_SPRITE_GROUP][collide_bottom].top) ) <= ( self.fall_speed + 2 ):
 					self.isCollidingBottom = True
 					self.isAirborne = False
 					self.fall_speed = 0
-					self.pos = (self.pos[0], FOREGROUND.scenery_rects[collide_bottom].top - self.rect.height/2 - 1)							
+					self.pos = (self.pos[0], [x.rect for x in FOREGROUND_SPRITE_GROUP][collide_bottom].top - self.rect.height/2 - 1)							
 			
 			if self.pos[1] < FLOOR_HEIGHT:
 				self.fall_speed += 2
@@ -206,6 +142,17 @@ class PlayerObject(CharacterObject):
 	def update(self, direction):
 		super(PlayerObject, self).update(direction)
 
+class ForegroundSurfaceSprite(pygame.sprite.Sprite):
+	def __init__(self, screen, image, starting_position):
+		pygame.sprite.Sprite.__init__(self)
+		self.screen = screen
+		self.image = image
+		self.rect = self.image.get_rect(topleft=starting_position)
+		
+	def update(self, direction, speed):
+		if direction == LEFT: self.rect = self.rect.move(speed, 0)
+		elif direction == RIGHT: self.rect = self.rect.move(-speed, 0)
+
 class ForegroundSurfaces():
 	def __init__(self, screen, images, starting_positions):
 		self.screen = screen		
@@ -228,13 +175,13 @@ class ForegroundSurfaces():
 			if self.scenery_rects[i].left < (SCREEN_WIDTH + SCREEN_WIDTH/2) and self.scenery_rects[i].right > ((-1) * SCREEN_WIDTH/2 ):
 				self.screen.blit(self.scenery_surfaces[i], self.scenery_rects[i])
 
-class BackgroundSurfaces():
+class BackgroundSurfaces(): #needs to be updated to handle a dynamic number of background images
 	def __init__(self, screen):
 		self.screen = SCREEN
 		self.panel_order = [0, 1, 2, 3, 4]
 		self.panel_x_pos = [ (-1)*SCREEN_WIDTH*2, (-1)*SCREEN_WIDTH, 0, SCREEN_WIDTH, SCREEN_WIDTH*2]
 		self.surfaces = []
-		path = r'C:/Python/ServiceGame/art_assets/'
+		path = ASSETS_PATH
 		for i in range(5):			
 			fn = 'b' + str(i+1) + '.jpg'
 			full_path = os.path.join(path, fn)
@@ -243,8 +190,7 @@ class BackgroundSurfaces():
 	
 	def updatePos(self, direction, speed):
 		"""
-		direction type is int
-		using RIGHT or LEFT
+		direction type is int using RIGHT or LEFT
 		speed type is int
 		determines change in position per call
 		"""
@@ -265,22 +211,91 @@ class BackgroundSurfaces():
 				self.screen.blit(surface, (self.panel_x_pos[self.panel_order[index]], 0))			
 	
 def loadImages(path, f_names):
+	"""returns a list of pygame.Surface objects, loaded from images at 'path' having a name in the 'f_names' list"""
 	ret_surfaces = []
 	for f_name in f_names:		
 		ret_surfaces.append( pygame.image.load(os.path.join(path, f_name)).convert_alpha() )
 	return ret_surfaces
 
-if __name__ == "__main__":
+if __name__ == "__main__": #Globals
+	
+	##directions for movement
+	LEFT = 0
+	RIGHT = 1
+	DOWN = 2
+	UP = 3
+	#next two are to avoid animation of non-player objects 
+	#when they are only moving due to player movement
+	PLAYER_LEFT_ONLY = 4 
+	PLAYER_RIGHT_ONLY = 5
+	##/directions for movement
+
+	##colors
+	GREY = (128, 128, 128)
+	RED = (255, 0, 0)
+	GREEN = (0, 255, 0)
+	BLUE = (0, 0, 255)
+	LIGHT_BLUE = (125, 125, 255)
+	##/colors
+
+	##speeds
+	BACKGROUND_SPEED = 10
+	FOREGROUND_SPEED = 8
+	##/speeds
+
+	##collision rect widths
+	LEFT_RECT_WIDTH = 5
+	RIGHT_RECT_WIDTH = 5
+	##
+
+	##universal scenery heights
+	#remember that (0, 0) in pygame is the top-left corner; y > 0 is below this point
+	FLOOR_HEIGHT = 400
+	##
+
+	##image array layout keys
+	#IDLE
+	IDLE_0 	= 0
+	IDLE_1 	= 1
+	IDLE_2 	= 2
+	IDLE_3 	= 3
+	#LEFT
+	LEFT_0 	= 4
+	LEFT_1 	= 5
+	LEFT_2 	= 6
+	LEFT_3 	= 7
+	#RIGHT
+	RIGHT_0 = 8
+	RIGHT_1 = 9
+	RIGHT_2 = 10
+	RIGHT_3 = 11
+	#UP
+	UP_0 	= 12
+	UP_1 	= 13
+	UP_2 	= 14
+	UP_3 	= 15
+	#DOWN
+	DOWN_0 	= 16
+	DOWN_1 	= 17
+	DOWN_2 	= 18
+	DOWN_3 	= 19
+	##
+	
+	##SCREEN surface attributes
 	SCREEN_WIDTH = 800
 	SCREEN_HEIGHT = 600	
 	SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	SCREEN_RECT = SCREEN.get_rect()
 	CLOCK = pygame.time.Clock()		
+	##
 	
+	##Bools for determining whether a key is held down
 	LEFT_KEY_DOWN = False
 	RIGHT_KEY_DOWN = False
-		
-	ASSETS_PATH = r'C:/Python/ServiceGame/art_assets/'
+	##
+	
+	##art assets and asset arrays
+	ASSETS_PATH = r'art_assets/'
 	
 	PLAYER_ASSET_FNAMES = [
 	'death_sprite_left_1.png',
@@ -339,6 +354,8 @@ if __name__ == "__main__":
 	(1400, 400),
 	(-800, 400),
 	(1800, 600)]
+	##
+	
 	
 	##loading images for objects	
 	foreground_surfaces = loadImages( ASSETS_PATH, FOREGROUND_ASSET_FNAMES )
@@ -347,8 +364,12 @@ if __name__ == "__main__":
 	##
 	
 	##instatiate objects
-	BACKGROUND = BackgroundSurfaces(SCREEN)	
-	FOREGROUND = ForegroundSurfaces(SCREEN, foreground_surfaces, FOREGROUND_ASSET_POSITIONS)
+	BACKGROUND = BackgroundSurfaces(SCREEN)	#old, should be switched to sprites
+	FOREGROUND = ForegroundSurfaces(SCREEN, foreground_surfaces, FOREGROUND_ASSET_POSITIONS) #old, should be switched to sprites
+	
+	FOREGROUND_SPRITE_GROUP = pygame.sprite.Group()
+	for index, forg_surf in enumerate(foreground_surfaces):
+		FOREGROUND_SPRITE_GROUP.add(ForegroundSurfaceSprite(SCREEN, forg_surf, FOREGROUND_ASSET_POSITIONS[index]))
 	
 	PLAYER = PlayerObject(SCREEN, player_object_surfaces, (SCREEN_WIDTH/2, FLOOR_HEIGHT))
 	PLAYER_SPRITE_GROUP = pygame.sprite.Group()
@@ -374,15 +395,16 @@ if __name__ == "__main__":
 	EVERY_CHARACTER_SPRITE_GROUP.add(AI_CHARACTER_4)
 	##	
 	
-	#char rand act
+	##ai character rand action vars, temporary
 	ai_rand_act = 0
-	char_action = 2
-	#remove later
+	char_action = 2	
 	
+if __name__ == "__main__": #main loop	
 	while 1:
 		time_elapsed = CLOCK.tick(30)
 				
-		#SCREEN.fill((0,0,0))	
+		#SCREEN.fill((0,0,0)) #not necessary currently as entire background is drawn to with no transparency
+		
 		event_list = pygame.event.get()
 		for event in event_list:
 			#KEYPRESS EVENTS
@@ -410,12 +432,14 @@ if __name__ == "__main__":
 		if LEFT_KEY_DOWN and PLAYER.isCollidingLeft == False: 
 			PLAYER_SPRITE_GROUP.update(LEFT)
 			BACKGROUND.updatePos(LEFT, BACKGROUND_SPEED)
-			FOREGROUND.updatePos(LEFT, FOREGROUND_SPEED)
+			#FOREGROUND.updatePos(LEFT, FOREGROUND_SPEED)
+			FOREGROUND_SPRITE_GROUP.update(LEFT, FOREGROUND_SPEED)
 			AI_CHARACTER_SPRITE_GROUP.update(PLAYER_LEFT_ONLY)
 		if RIGHT_KEY_DOWN and PLAYER.isCollidingRight == False: 
-			BACKGROUND.updatePos(RIGHT, BACKGROUND_SPEED)
-			FOREGROUND.updatePos(RIGHT, FOREGROUND_SPEED)
 			PLAYER_SPRITE_GROUP.update(RIGHT)
+			BACKGROUND.updatePos(RIGHT, BACKGROUND_SPEED)
+			#FOREGROUND.updatePos(RIGHT, FOREGROUND_SPEED)
+			FOREGROUND_SPRITE_GROUP.update(RIGHT, FOREGROUND_SPEED)			
 			AI_CHARACTER_SPRITE_GROUP.update(PLAYER_RIGHT_ONLY)
 			
 		#character random action		
@@ -438,25 +462,11 @@ if __name__ == "__main__":
 		BACKGROUND.drawBackgrounds()		
 		pygame.draw.rect(SCREEN, GREY, ((0, FLOOR_HEIGHT), (SCREEN_WIDTH, SCREEN_HEIGHT-FLOOR_HEIGHT)) )		
 		
-		'''
-		##collision rect testing				
-		pygame.draw.rect(SCREEN, LIGHT_BLUE, PLAYER.rect)
-		pygame.draw.rect(SCREEN, LIGHT_BLUE, AI_CHARACTER.rect)
-		
-		pygame.draw.rect(SCREEN, RED, PLAYER.bottom_collision_rect)
-		pygame.draw.rect(SCREEN, RED, PLAYER.right_side_collision_rect)
-		pygame.draw.rect(SCREEN, RED, PLAYER.left_side_collision_rect)
-		
-		pygame.draw.rect(SCREEN, GREEN, AI_CHARACTER.bottom_collision_rect)
-		pygame.draw.rect(SCREEN, GREEN, AI_CHARACTER.right_side_collision_rect)
-		pygame.draw.rect(SCREEN, GREEN, AI_CHARACTER.left_side_collision_rect)
-		##/collision rect testing
-		'''
-		
 		AI_CHARACTER_SPRITE_GROUP.draw(SCREEN)		
 		PLAYER_SPRITE_GROUP.draw(SCREEN)		
 		
-		FOREGROUND.drawForegroundSurfaces()
+		#FOREGROUND.drawForegroundSurfaces()
+		FOREGROUND_SPRITE_GROUP.draw(SCREEN)
 		
 		#display new frame
 		pygame.display.flip()
