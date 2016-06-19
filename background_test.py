01b9aebf4cdfd162d849747159edf75344fe7232
jonnykantor <jonnykantor@gmail.com>
2016-06-19 08:31:23 -0700

    Daily commit for continued work away from desktop

--- background_test.py
+++ background_test.py
@@ -168,7 +168,7 @@ class BackgroundSprite(ScenerySprite):
 		super(BackgroundSprite, self).update(direction, speed)	
 
 class BackgroundSurfacesManager(object):
-	def __init__(self, screen, image_list, starting_positions, scroll_limits=None ):
+	def __init__(self, screen, image_list, starting_positions, wrap=True, scroll_limits=None ):
 		"""
 		Managing object for the sprite.Group for all background surfaces.
 		This is mainly to handle the unique movement involved with the backgrounds
@@ -185,15 +185,34 @@ class BackgroundSurfacesManager(object):
 		with updating the image positions in the case of a wrapping background
 		"""
 		self.screen = screen
-		self.images = image_list
-		self.image_order = range(len(image_list))
-		self.positions = starting_positions
-		self.limits = scroll_limits
+		self.images = image_list 
+		self.image_order = range(len(image_list)) 	#in case of wrap=True, will form a circular list, indicating which image will be drawn in what order
+		self.positions = starting_positions 		#list of tuples [(x-coord, y-coord),...]
+		self.limits = scroll_limits 				#tuple of 2 x-values (left-limit, right-limit)
+		self.wrap = wrap 							#if true: background is circular in nature - will wrap back around
+		self.limit_test_position = None
+		if self.limits != None: self.limit_test_position = SCREEN_WIDTH/2
+		
 		self.background_sprite_group = pygame.sprite.Group()
 		for index, image in enumerate(self.images): 
 			background_sprite_group.add(BackgroundSprite(self.screen, image, positions[index]))
-
-
+			
+	def update(self, direction, speed):
+		"""updates all background sprites uniformly, unless doing so would violate a limit in self.limits""" 
+		if direction == LEFT:		
+		
+		if direction == LEFT:
+			if (self.limits != None): if (self.limits[1] <= (self.limit_test_position + speed)): return None
+			self.positions = [x+speed for x in self.positions]
+			
+			if self.positions[0] >= (-1)*SCREEN_WIDTH: 
+				self.panel_order = self.panel_order[1:] + self.panel_order[:1]
+				self.panel_x_pos = [x-SCREEN_WIDTH for x in self.panel_x_pos]
+		elif direction == RIGHT: 
+			self.panel_x_pos = [x-speed for x in self.panel_x_pos]
+			if self.panel_x_pos[0] < (-1)*SCREEN_WIDTH*3:
+				self.panel_order = self.panel_order[-1:] + self.panel_order[:-1]
+				self.panel_x_pos = [x+SCREEN_WIDTH for x in self.panel_x_pos]
 
 class BackgroundSurfaces(): #needs to be updated to handle a dynamic number of background images
 	def __init__(self, screen):
